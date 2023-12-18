01e7237314e13a0dcd05b924d58b9f7f15638f17
Arnau Saborit <arnausv@uoc.edu>
2023-12-18 11:03:36 +0100

    First review passed

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,34 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+
 /**
  * Write any other JavaScript below
  */
+
+/* Boilerplate default JS code
 
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+*/
+
+//Navbar link active class
+
+$(document).ready(function() {
+  var currentPage = window.location.pathname.split('/').pop();
+
+  $('.nav a').removeClass('text-slate-600 text-default_green');
+
+  if (currentPage === 'index.html' || currentPage === '') {
+    $('.nav a[href="index.html"]').addClass('text-default_green');
+  } else {
+    $('.nav a[href="' + currentPage + '"]').addClass('text-default_green');
+  }
+});
+
+//Initialize Aos library
+
+AOS.init();
